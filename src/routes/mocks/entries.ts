56cc65becdd0449e23694ec5a92ad332793bd60f
m208 <avcvet@yandex.ru>
2023-01-29 21:03:19 +0300

    feat: add creating mocked users and content

---
+++ src/routes/mocks/entries.ts
@@ -0,0 +1,48 @@
+export const mockedUsers = [
+    {
+        "firstName": "Petr",
+        "lastName": "Petrov",
+        "email": "user1@mail.com",
+    },
+    {
+        "firstName": "Ivan",
+        "lastName": "Ivanov",
+        "email": "user2@mail.com",
+    },
+    {
+        "firstName": "Semen",
+        "lastName": "Semenov",
+        "email": "user3@mail.com",
+    }
+]
+
+export const mockedProfiles = [
+    {
+        "avatar": "pic1.png",
+        "sex": "M",
+        "birthday": 2000,
+        "country": "Sweden",
+        "street": "street",
+        "city": "city",
+        "memberTypeId" : "basic"
+    },
+    {
+        "avatar": "pic2.png",
+        "sex": "M",
+        "birthday": 3000,
+        "country": "Norway",
+        "street": "street",
+        "city": "city",
+        "memberTypeId" : "basic"
+    },
+    {
+        "avatar": "pic11112.png",
+        "sex": "F",
+        "birthday": 78978987,
+        "country": "Turkey",
+        "street": "street",
+        "city": "city",
+        "memberTypeId" : "business"
+    }
+]
+
